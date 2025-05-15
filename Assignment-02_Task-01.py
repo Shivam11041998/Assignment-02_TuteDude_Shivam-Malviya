2bde834fc429e91b678060c975e574e43112d034
Shivam11041998 <shivammalviya0411@gmail.com>
2025-05-15 14:36:21 +0530

    Add files via upload

---
+++ Assignment-02_Task-01.py
@@ -0,0 +1,13 @@
+#Check if a Number is even or Odd (a=7)
+a=int(input('Enter a number: '))
+if a%2==0:
+    print(a,'is a even number.')
+else:
+    print(a,'is a odd number.')
+
+# Check if a Number is even or Odd (a=12)
+a=int(input('Enter a number: '))
+if a%2==0:
+    print(a,'is a even number.')
+else:
+    print(a,'is a odd number.')
---
+++ Assignment-02_Task-02.py
@@ -0,0 +1,5 @@
+Sum=0
+for a in range(1,51):
+    Sum+=a
+print('The sum of integer from 1 to 50 is:',Sum)
+
